fddf5583738973da200f3af52b7bb3ccb55e4914
Samrat Bose <samratofficial2009@gmail.com>
2024-07-04 21:55:07 +0530

    basic things done

--- script.js
+++ script.js
@@ -4,4 +4,30 @@ function playMusic() {
     const audioSource = document.getElementById('audioSource');
 
     const music = {
-        happy: '
+        happy: 'ganna/khush.mp3',
+        sad: 'ganna/dhuki.mp3',
+        relaxed: 'ganna/relax.mp3',
+        energetic: 'ganna/energywla.mp3'
+
+    };
+
+    if (music[mood]) {
+        audioSource.src = music[mood];
+        audioPlayer.load();
+        audioPlayer.play();
+    } else {
+        audioPlayer.pause();
+        audioPlayer.src = '';
+    }
+}
+
+function pauseMusic() {
+    const audioPlayer = document.getElementById('audioPlayer');
+    audioPlayer.pause();
+}
+
+function stopMusic() {
+    const audioPlayer = document.getElementById('audioPlayer');
+    audioPlayer.pause();
+    audioPlayer.currentTime = 0;
+}
